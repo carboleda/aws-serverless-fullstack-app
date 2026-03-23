3fef0001b2ef084c0ff66ffc0b5ae9dd9557f98f
Carlos F Arboleda G <arbofercho@gmail.com>
2026-03-22 22:39:46 -0500

    feat: add DynamoDB data source and environment configuration with decorators

---
+++ backend/src/shared/database/dynamodb.ts
@@ -0,0 +1,29 @@
+import Envs from "@/shared/helpers/envs";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import { Injectable } from "@/shared/decorators/tsyringe.decorator";
+
+@Injectable()
+export default class DynamoDBDataSource {
+  private db: DynamoDBDocumentClient | null = null;
+
+  public getConnection(): DynamoDBDocumentClient {
+    if (!this.db) {
+      const offlineOptions = {
+        region: Envs.DYNAMODB_LOCAL_REGION,
+        endpoint: Envs.DYNAMODB_LOCAL_ENDPOINT,
+        credentials: {
+          accessKeyId: Envs.AWS_ACCESS_KEY_ID,
+          secretAccessKey: Envs.AWS_SECRET_ACCESS_KEY,
+        },
+      };
+
+      const client = new DynamoDBClient(Envs.IS_OFFLINE ? offlineOptions : {});
+      this.db = DynamoDBDocumentClient.from(client, {
+        marshallOptions: { removeUndefinedValues: true },
+      });
+    }
+
+    return this.db;
+  }
+}
---
+++ backend/src/shared/helpers/envs.ts
@@ -0,0 +1,17 @@
+import process from "node:process";
+
+export default class Envs {
+  public static readonly NODE_ENV: string = process.env.NODE_ENV!!;
+  public static readonly IS_OFFLINE: Boolean = JSON.parse(
+    process.env.IS_OFFLINE || "false",
+  );
+
+  public static readonly DYNAMODB_LOCAL_REGION: string =
+    process.env.DYNAMODB_LOCAL_REGION || process.env.AWS_DEFAULT_REGION || "";
+  public static readonly DYNAMODB_LOCAL_ENDPOINT: string =
+    process.env.DYNAMODB_LOCAL_ENDPOINT || "";
+  public static readonly AWS_ACCESS_KEY_ID: string =
+    process.env.AWS_ACCESS_KEY_ID || "";
+  public static readonly AWS_SECRET_ACCESS_KEY: string =
+    process.env.AWS_SECRET_ACCESS_KEY || "";
+}
